38c324770bcb54fcbd700343979d6570c19380a9
jy0t1 <nimisha.sarkar@outlook.com>
2024-11-16 01:10:26 -0500

    all done

--- whackamole.py
+++ whackamole.py
@@ -1,5 +1,5 @@
 import pygame
-
+import random
 
 def main():
     try:
@@ -10,11 +10,25 @@ def main():
         screen = pygame.display.set_mode((640, 512))
         clock = pygame.time.Clock()
         running = True
+        mole_pos = (0,0)
         while running:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     running = False
-            screen.fill("light green")
+                elif event.type == pygame.MOUSEBUTTONDOWN:
+                    mouse_x, mouse_y = event.pos
+                    mole_x, mole_y = mole_pos
+                    print(f"Mouse clicked at: ({mouse_x}, {mouse_y})")
+                    print(f"Mole is at: ({mole_x}, {mole_y})")
+                    if (mole_x <= mouse_x < mole_x + 32) and (mole_y <= mouse_y < mole_y + 32):
+                        mole_pos = (random.randint(0, 19) * 32, random.randint(0, 15) * 32)
+            screen.fill("light blue")
+            xVal = 0
+            for x in range(0, 640, 32):
+                pygame.draw.line(screen, "black", (x, 0), (x, 512))
+            for y in range(0, 512, 32):
+                pygame.draw.line(screen, "black", (0, y), (640, y))
+            screen.blit(mole_image, mole_image.get_rect(topleft=mole_pos))
             pygame.display.flip()
             clock.tick(60)
     finally:
